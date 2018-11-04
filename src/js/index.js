722a7ec87147d2f461e4d74ef6d2f78cf108f7ee
Jim <jimfrenette@yahoo.com>
2018-11-04 08:43:44 -0500

    add lightbox and slick

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 import style from '../sass/style.scss'
-
 import Lazyimage from './lazyimage'
+import Lightbox from './lightbox'
 
 new Lazyimage();
+
+new Lightbox({
+    name: 'lightbox',
+    source: '.photogrid',
+    opener: 'li',
+    type: 'slider'
+});
---
+++ src/js/lightbox.js
@@ -0,0 +1,104 @@
+import $ from 'jquery'
+import 'slick-carousel'
+
+export default class Lightbox {
+    constructor(options) {
+
+        this.settings = $.extend({/* defaults */}, options);
+
+        this.init();
+    }
+
+    init() {
+
+        let source = $(this.settings.source);
+
+        if (source.length) {
+
+            source.each((index, el) => {
+
+                this.create(index, el);
+            });
+        }
+    }
+
+    create(index, el) {
+
+        let lightbox = this.settings.name + '__' + index,
+            opener = $(el).find(this.settings.opener);
+
+        $('body').append('<div data-lightbox="' + lightbox + '" class="lightbox"><div></div></div>');
+
+        if (this.settings.type === 'slider') {
+
+            $('div[data-lightbox="' + lightbox + '"] > div')
+                .append('<div class="lightbox-slider"></div>');
+
+            var slider = $('div[data-lightbox="' + lightbox + '"] .lightbox-slider');
+
+            slider.slick({
+                dots: true
+            });
+
+            opener.each((index, el) => {
+                this.popSlider(lightbox, slider, el);
+            });
+        }
+
+        // close button
+        $('div[data-lightbox="' + lightbox + '"] > div')
+            .prepend('<a class="lightbox-close" href="javascript:void(0)">+</a>');
+
+        $('.lightbox-close').on( 'click', function() {
+            $('[data-lightbox="' + lightbox + '"]').removeClass('is-open');
+        });
+
+        //close on outside click
+        window.onclick = function(evt) {
+            if (evt.target.dataset.lightbox == lightbox) {
+                $('[data-lightbox="' + lightbox + '"]').removeClass('is-open');
+            }
+        }
+
+        // close on escape
+        $(document).keyup(function(evt) {
+            if (evt.which === 27) {
+                $('[data-lightbox="' + lightbox + '"]').removeClass('is-open');
+            }
+        });
+
+    }
+
+    popSlider(lightbox, slider, el) {
+
+        let img = $(el).find('img'),
+            src = img.prop('src'),
+            slide = document.createElement('div'),
+            slideImg = document.createElement('img');
+
+        slideImg.src = src;
+
+        slide.appendChild(slideImg);
+
+        if (img.attr('alt')) {
+            let caption = document.createElement('p'),
+                captionText = document.createTextNode(img.attr('alt'));
+
+            caption.appendChild(captionText);
+            slide.appendChild(caption);
+        }
+
+        slider.slick('slickAdd', slide);
+
+        img.wrap('<a href="' + src + '"></a>').on( 'click', function(evt) {
+
+            evt.preventDefault();
+
+            $('[data-lightbox="' + lightbox + '"]').addClass('is-open');
+
+            let index = $(this).closest(el).index();
+
+            slider.slick('slickGoTo', index);
+        });
+    }
+}
